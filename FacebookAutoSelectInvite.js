cd7d37487120ba3d45173e07897e8df8a6b01f61
Amin Yusuf <si.logoz@gmail.com>
2024-06-21 00:23:47 +0700

    Create FacebookAutoSelectInvite.js

---
+++ FacebookAutoSelectInvite.js
@@ -0,0 +1,34 @@
+function autoInviteFacebook() {
+  const titleModalInvite = "Invite followers to this group"; // This depend on your language at facebook
+
+  let countTry = 0;
+  const modalElm = document.querySelector(
+    `[role="dialog"][aria-label="${titleModalInvite}"]`
+  );
+
+  if (!modalElm) {
+    throw new Error("Modal invite not found");
+  }
+
+  function selectCheckBox() {
+    const checkBoxs = modalElm.querySelectorAll('[role="checkbox"]');
+    const checkBoxsUnchecked = [...checkBoxs].filter(
+      (e) => e.ariaChecked === "false" && e.tabIndex === 0
+    );
+
+    if (checkBoxsUnchecked.length === 0) {
+      if (countTry > 3) return false;
+      countTry += 1;
+      setTimeout(selectCheckBox, 1500);
+    }
+
+    countTry = 0;
+
+    checkBoxs.item(checkBoxs.length - 1).scrollIntoView();
+    checkBoxsUnchecked[0].click();
+
+    setTimeout(selectCheckBox, Math.random() * 300);
+  }
+
+  selectCheckBox();
+}
